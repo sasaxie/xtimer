1d20662abc388179d8c6806a41127f0723421228
XiaodongXie <xiexiaodong917@163.com>
2017-03-23 11:44:53 +0800

    :art:格式化代码

--- xtimer.go
+++ xtimer.go
@@ -5,50 +5,49 @@
 package xtimer
 
 import (
-    "time"
+	"time"
 )
 
 /*
  * Execute your logic until program was done or stop this function.
  */
 func StartTicker(interval time.Duration, f func(now time.Time)) chan bool {
-    done := make(chan bool, 1)
+	done := make(chan bool, 1)
 
-    go func() {
-        t := time.NewTicker(interval)
-        defer t.Stop()
-        for {
-            select {
-            case now := <-t.C:
-                f(now)
-            case <-done:
-                return
-            }
-        }
-    }()
+	go func() {
+		t := time.NewTicker(interval)
+		defer t.Stop()
+		for {
+			select {
+			case now := <-t.C:
+				f(now)
+			case <-done:
+				return
+			}
+		}
+	}()
 
-    return done
+	return done
 }
 
 /*
  * Regularly executed once.
  */
 func StartTimer(interval time.Duration, f func(now time.Time)) chan bool {
-    done := make(chan bool, 1)
+	done := make(chan bool, 1)
 
-    go func() {
-        t := time.NewTimer(interval)
-        defer t.Stop()
-        for {
-            select {
-            case now := <-t.C:
-                f(now)
-            case <-done:
-                return
-            }
-        }
-    }()
+	go func() {
+		t := time.NewTimer(interval)
+		defer t.Stop()
+		for {
+			select {
+			case now := <-t.C:
+				f(now)
+			case <-done:
+				return
+			}
+		}
+	}()
 
-    return done
+	return done
 }
-
--- xtimer_test.go
+++ xtimer_test.go
@@ -1,45 +1,45 @@
 package xtimer
 
 import (
-    "testing"
-    "time"
-    "log"
+	"log"
+	"testing"
+	"time"
 )
 
 func TestStartTicker(t *testing.T) {
-    ticker := StartTicker(time.Second, func(now time.Time) {
-        log.Println("Time:", now.Format("2006-01-02 15:04:05"))
-    })
+	ticker := StartTicker(time.Second, func(now time.Time) {
+		log.Println("Time:", now.Format("2006-01-02 15:04:05"))
+	})
 
-    i := 0
-    for {
-        i++
-        if i == 10 {
-            // Stop ticker
-            close(ticker)
-            break
-        }
-        time.Sleep(time.Second)
-    }
+	i := 0
+	for {
+		i++
+		if i == 10 {
+			// Stop ticker
+			close(ticker)
+			break
+		}
+		time.Sleep(time.Second)
+	}
 
-    log.Println("end")
+	log.Println("end")
 }
 
 func TestStartTimer(t *testing.T) {
-    timer := StartTimer(time.Second, func(now time.Time) {
-        log.Println("Time:", now.Format("2006-01-02 15:04:05"))
-    })
+	timer := StartTimer(time.Second, func(now time.Time) {
+		log.Println("Time:", now.Format("2006-01-02 15:04:05"))
+	})
 
-    i := 0
-    for {
-        i++
-        if i == 10 {
-            // Stop timer
-            close(timer)
-            break
-        }
-        time.Sleep(time.Second)
-    }
+	i := 0
+	for {
+		i++
+		if i == 10 {
+			// Stop timer
+			close(timer)
+			break
+		}
+		time.Sleep(time.Second)
+	}
 
-    log.Println("end")
+	log.Println("end")
 }
