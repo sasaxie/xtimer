67f52fb20732aa9bc40d4a9224e38bbf120f2fa9
XiaodongXie <xiexiaodong917@163.com>
2017-03-23 11:44:13 +0800

    :rocker:计划任务

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # xtimer
 Golang timer tools
+
+## Installation
+> go get github.com/Vickixiaodong/xtimer
+
+## 功能
+- [x] StartTimer(interval, func(time))：定时执行一次逻辑
+- [x] StartTicker(interval, func(time))：定时执行多次逻辑，直到主动关闭或者程序结束。
---
+++ xtimer.go
@@ -0,0 +1,54 @@
+/**
+ * Timer for scheduled tasks.
+ */
+
+package xtimer
+
+import (
+    "time"
+)
+
+/*
+ * Execute your logic until program was done or stop this function.
+ */
+func StartTicker(interval time.Duration, f func(now time.Time)) chan bool {
+    done := make(chan bool, 1)
+
+    go func() {
+        t := time.NewTicker(interval)
+        defer t.Stop()
+        for {
+            select {
+            case now := <-t.C:
+                f(now)
+            case <-done:
+                return
+            }
+        }
+    }()
+
+    return done
+}
+
+/*
+ * Regularly executed once.
+ */
+func StartTimer(interval time.Duration, f func(now time.Time)) chan bool {
+    done := make(chan bool, 1)
+
+    go func() {
+        t := time.NewTimer(interval)
+        defer t.Stop()
+        for {
+            select {
+            case now := <-t.C:
+                f(now)
+            case <-done:
+                return
+            }
+        }
+    }()
+
+    return done
+}
+
